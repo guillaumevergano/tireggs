571a8434e6a213a2ce3fa088eceab1d32f7a94fd
GoldSenseiBoi <98558347+GoldSenseiBoi@users.noreply.github.com>
2024-01-06 01:19:32 +0100

    add :anchor:

--- tireggs-min.js
+++ tireggs-min.js
@@ -8,15 +8,19 @@
             },
             trigger: {
                 selector: '',
-                event: 'click'
+                event: 'click' // Gardons le click comme événement par défaut
             },
             position: {
                 div: '',
                 at: 'center center'
             },
             animation: {
-                type: 'move', // Par défaut, l'effet d'animation est défini sur 'move'
+                type: 'move',
                 direction: 'rightleft'
+            },
+            apparition: {
+                type: 'fade',
+                duration: 1000
             }
         };
 
@@ -24,73 +28,83 @@
 
         var elements = document.querySelectorAll(settings.trigger.selector);
         elements.forEach(function(element) {
-            element.addEventListener(settings.trigger.event, function() {
-                var img = new Image();
-                img.src = settings.image.url;
-                img.style.width = settings.image.width;
-                img.style.height = settings.image.height;
-                img.style.position = 'absolute';
-                
-                var parent = document.querySelector(settings.position.div);
-                parent.appendChild(img);
+            if (settings.trigger.event === 'click') {
+                element.addEventListener('click', function() {
+                    triggerAnimation(settings);
+                });
+            } else if (settings.trigger.event === 'hover') {
+                element.addEventListener('mouseenter', function() {
+                    triggerAnimation(settings);
+                });
+            }
+        });
 
-                var startValue = '100%';
-                var endValue = '-100%';
-                var property = 'left';
+        function triggerAnimation(settings) {
+            var img = new Image();
+            img.src = settings.image.url;
+            img.style.width = settings.image.width;
+            img.style.height = settings.image.height;
+            img.style.position = 'absolute';
+            
+            var parent = document.querySelector(settings.position.div);
+            parent.appendChild(img);
 
-                switch (settings.animation.direction) {
-                    case 'rightleft':
-                        startValue = '100%';
-                        endValue = '-100%';
-                        property = 'left';
-                        break;
-                    case 'leftright':
-                        startValue = '-100%';
-                        endValue = '100%';
-                        property = 'left';
-                        break;
-                    case 'bottomtop':
-                        startValue = '100%';
-                        endValue = '-100%';
-                        property = 'top';
-                        break;
-                    case 'topbottom':
-                        startValue = '-100%';
-                        endValue = '100%';
-                        property = 'top';
-                        break;
-                    default:
-                        break;
-                }
+            var startValue = '100%';
+            var endValue = '-100%';
+            var property = 'left';
 
-                img.style[property] = startValue;
+            switch (settings.animation.direction) {
+                case 'rightleft':
+                    startValue = '100%';
+                    endValue = '-100%';
+                    property = 'left';
+                    break;
+                case 'leftright':
+                    startValue = '-100%';
+                    endValue = '100%';
+                    property = 'left';
+                    break;
+                case 'bottomtop':
+                    startValue = '100%';
+                    endValue = '-100%';
+                    property = 'top';
+                    break;
+                case 'topbottom':
+                    startValue = '-100%';
+                    endValue = '100%';
+                    property = 'top';
+                    break;
+                default:
+                    break;
+            }
 
-                if (settings.animation.type === 'fixed') {
+            img.style[property] = startValue;
+
+            if (settings.animation.type === 'fixed') {
+                setTimeout(function() {
+                    img.style.transition = `opacity ${settings.apparition.duration / 1000}s linear, ${property} ${settings.apparition.duration / 1000}s linear`;
+                    img.style[property] = '0';
+                    img.style.opacity = '0';
+
                     setTimeout(function() {
-                        img.style.transition = 'opacity 1s linear';
-                        img.style[property] = '0';
-                        img.style.opacity = '0';
+                        img.remove();
+                    }, settings.apparition.duration);
+                }, 3000);
+            } else {
+                setTimeout(function() {
+                    img.style.transition = `${property} ${settings.apparition.duration / 1000}s linear`;
+                    img.style[property] = '0';
+
+                    setTimeout(function() {
+                        img.style.transition = `${property} ${settings.apparition.duration / 1000}s linear`;
+                        img.style[property] = endValue;
 
                         setTimeout(function() {
                             img.remove();
-                        }, 1000);
-                    }, 3000);
-                } else {
-                    setTimeout(function() {
-                        img.style.transition = `${property} 6s linear`;
-                        img.style[property] = '100%';
-
-                        setTimeout(function() {
-                            img.style.transition = `${property} 4s linear`;
-                            img.style[property] = endValue;
-
-                            setTimeout(function() {
-                                img.remove();
-                            }, 9000);
-                        }, 2000);
+                        }, settings.apparition.duration * 2);
                     }, 0);
-                }
-            });
-        });
+                }, 0);
+            }
+        }
     };
 })();
